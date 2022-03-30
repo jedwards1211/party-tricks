82a820f4ac8c77f7766370aa682b8fd08a0b4e0a
Andy Edwards <jedwards@fastmail.com>
2022-03-30 11:30:24 -0500

    Update bouncyCastle.js

--- bouncyCastle.js
+++ bouncyCastle.js
@@ -2,10 +2,4 @@ var animation = document.createElement('style')
 animation.textContent = '@keyframes bouncy { from { transform: scale(0.95); } to { transform: scale(1.05); } }'
 document.head.appendChild(animation)
 
-document.querySelectorAll('*').forEach(function (elem) {
-  elem.style.animationName = 'bouncy'
-  elem.style.animationDuration = (Math.random() + 0.5) + 's'
-  elem.style.animationTimingFunction = 'ease'
-  elem.style.animationIterationCount = 'infinite'
-  elem.style.animationDirection = 'alternate-reverse' 
-})
+document.querySelectorAll('*').forEach(e => e.style.animation = `bouncy ${Math.random() + 0.5}s ease infinite alternate-reverse`)
